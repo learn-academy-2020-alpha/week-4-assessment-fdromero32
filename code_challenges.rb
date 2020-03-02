7d375415c3d43fd9c0ccb20b9e4d2a6ab3b882d6
fdromero32 <60331306+fdromero32@users.noreply.github.com>
2020-03-01 21:54:21 -0800

    reuploading bc i deleted everything

--- code_challenges.rb
+++ code_challenges.rb
@@ -1,49 +1,89 @@
-# ASSESSMENT 4: Ruby Coding Practical Questions
-
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest.
 
-
-
-
-
 fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
-# Expected output: [-9, 7, 9, 199]
+
 fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
-# Expected output: [-823, 7, 23]
 
+def array_sorter(arr)
+    new_arr2 = []
+    new_arr = arr.delete_if { |obj| (obj.is_a? String) || (obj.is_a? TrueClass) || (obj.is_a? FalseClass)|| (obj.is_a? NilClass)}
+    
+    new_arr.each { |value| 
+    if(value % 2 == 1 || value % 2 == -1) 
+        new_arr2.push(value)
+    end
+}
+new_arr2.sort
+end
 
-# --------------------2) Create a class called Bike that is initialized with a model, wheels, and frame size. Create a get_info method that returns a sentence with all the data from the bike object.
+# p array_sorter(fullArr1)
+# p array_sorter(fullArr2)
 
+#--------------------2) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data from the bike object.
+class Bike
+    def initialize(model, wheels, frame_size)
+        @model = model
+        @wheels = 2
+        @frame_size = frame_size
+        @bell = "off"
+# -------------------4) Add a speedometer to the Bike class. The speed should be initialized at 0.
+        @speed = 0
+    end
 
+    def model
+        @model
+    end
 
+    def frame_size
+        @frame_size
+    end
 
+    def speed
+        @speed
+    end
 
-# Expected output example: 'The Trek bike has two wheels and a 168cm frame.'
-
-
+    def bike_info
+        "The #@model bike has #@wheels wheels and a #@frame_size frame."
+    end
 # -------------------3) Add a bell to the bike class and create a method that will ring the bell when the method is called.
 
-
-
-
-
-# Expected output example: my_bike.ring_bell => 'Beep beep!'
-
-
-# -------------------4) Add a speedometer to the Bike class. The speed should be initialized at 0.
-
-
-
-
-
-# Expected output example: my_bike.speed => 0
-
+    def bell_sound
+    if @bell == "off"
+            puts @bell = "RING RING RING RING"
+    else 
+        puts  @bell = "off"
+    end
+    end
 
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
+    def pedal_faster
+        @speed += 10
+        puts "Your speed is #@speed mph!"
+    end
 
+    def brake
+        if @speed > 0
+            @speed -=5
+            p "Your slowing down to #@speed mph!"
+        else 
+            @speed = 0
+            p "You're not moving!"
+        end
+    end
 
+end
 
+# mountain_bike = Bike.new("Mountain Thunder", 2, "190cm")
+kiddy_bike = Bike.new("Training Bike", 2, "130cm")
 
-
-# Expected output example: my_bike.pedal_faster 10 => 10
-# Expected output example: my_bike.brake 15 => 0
+kiddy_bike.bell_sound
+p kiddy_bike.speed
+kiddy_bike.pedal_faster
+kiddy_bike.pedal_faster
+kiddy_bike.brake
+kiddy_bike.brake
+kiddy_bike.brake
+kiddy_bike.brake
+kiddy_bike.brake
+kiddy_bike.brake
+# p mountain_bike.bike_info
--- coding-challenges.js
+++ coding-challenges.js
@@ -2,59 +2,98 @@
 
 // --------------------1) Create a function that takes in an array. Each time the function is run, remove the first item from the array and shuffle the remaining content. If the array is empty, return "The array is empty."
 
-
-
-
-
 var collections = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example1 (can be a different order): ["yellow", "blue", "pink", "green"]
-// Expected output example2 (can be a different order): ["blue", "green", "pink"]
-// Expected output example3 (can be a different order): ["pink", "green"]
+
+const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+     // While there remain elements to shuffle...
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+      // shift the first value and then randomize the array
+      array.shift()
+      if (array.length === 0){
+        return "empty"
+      } else if (array.length > 0){
+          return array
+      } else {
+          return "please input a valid array"
+      }
+  }
+
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
 
 
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
-
-
-
-
 
 var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+const cubeSum = arr => {
+    const mapArr = arr.map(value => Math.pow(value, 3))
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return mapArr.reduce(reducer)
+}
+// console.log(cubeSum(cubeAndSum1))
+// console.log(cubeSum(cubeAndSum2))
 
 
 // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
-
-
-
-
 
 var nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
+const minMaxFinder = array => {
+    newArr = []
+    newArr.push(Math.max(...array))
+    newArr.unshift(Math.min(...array))
+    return newArr
+}
+// console.log(minMaxFinder(nums1))
+// console.log(minMaxFinder(nums2))
 
 // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
-
-
-
-
 
 var testString1 = "albatross"
 // Expected output: "aLbAtRoSs"
 var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
+const firstLetterUppercase = (str) => {
+    var newString = "";
+    for (i=0; i < str.length; i++) {
+       newString += i % 2 == 0 ? str.charAt(i).toUpperCase() : str.charAt(i);
+    }
+    return newString;  
+  }
+
+// console.log(firstLetterUppercase(testString1))
+// console.log(firstLetterUppercase(testString2))
 
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator.
 
-
-
-
-
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+const noDupes = (arr1, arr2) =>{
+
+    const dupesArray = [...arr1, ...arr2]
+    var uniqueItems = Array.from(new Set(dupesArray))
+    return uniqueItems
+    
+    }
+
+// console.log(noDupes(testArr1, testArr2)) 
